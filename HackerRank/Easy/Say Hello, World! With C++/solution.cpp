263a560fba1df5cdb129e25b62099f5e6c70d76a
Sujay Borse <sujayborse09@gmail.com>
2026-09-11 16:08:43 +0530

    Add HackerRank Easy - Say "Hello, World!" With C++

---
+++ HackerRank/Easy/Say Hello, World! With C++/solution.cpp
@@ -0,0 +1,13 @@
+// HackerRank Problem: Say "Hello, World!" With C++
+// Link: https://www.hackerrank.com/challenges/cpp-hello-world/problem
+// Difficulty: Easy
+// Language: cpp
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
